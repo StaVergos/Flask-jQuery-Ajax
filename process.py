c1f7a7c89cceb7c2bb49cae00f79d6dc2dcee4b8
Stavros Vergos <stavrospvergos@gmail.com>
2020-03-24 17:07:18 +0200

    initial commit

--- process.py
+++ process.py
@@ -4,3 +4,20 @@ app = Flask(_name__)
 
 @app.route('/')
 def index():
+    return render_template('form.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+
+    email = request.form['email']
+    name = request.form['name']
+
+    if name and email:
+        newName = name[::-1]
+
+        return jsonify({'name': newName})
+
+    return jsonify({'error': 'Missing data'})
+
+if (__name__) == '__main__':
+    app.run(debug=True)
---
+++ static/js/form.js
@@ -0,0 +1,26 @@
+$(document).read(function() {
+
+    $('form').on('submit', function(event) {
+
+        $.ajax({
+            data: {
+                name: $('#nameInput').val(),
+                email: $('#emailInput').val()
+            },
+            type: 'POST'
+            url: '/process'
+        })
+        .done(function(data) {
+            if (data.error) {
+                $('#errorAlert').text(data.error).show();
+                $('#successAlert').hide();
+            }
+            else {
+                $('#successAlert').text(data.name).show();
+                $('#errorAlert').hide();
+            }
+        });
+
+        event.preventDefault();
+    });
+});
